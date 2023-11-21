6fdc9cfc81d4c6f97eddb378cc8d03e244884d9b
poojaj08092002 <142136667+poojaj08092002@users.noreply.github.com>
2023-11-21 19:11:00 +0530

    task 2 uploaded successfully

---
+++ Task 2/app.py
@@ -0,0 +1,50 @@
+import os
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('f.html')
+
+@app.route('/', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        # Retrieve form data
+        business_name = request.form['business_name']
+        about_bussiness = request.form['about_bussiness']
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        whatsapp = request.form['whatsapp']
+        instagram = request.form['instagram']
+        facebook = request.form['facebook']
+        twitter = request.form['twitter']
+        website = request.form['website']
+        address = request.form['address']
+
+        # Handle file upload
+        if 'photo' in request.files:
+            file = request.files['photo']
+
+            # Create the 'static/uploads/' directory if it doesn't exist
+            uploads_dir = os.path.join(app.root_path, 'static', 'uploads')
+            os.makedirs(uploads_dir, exist_ok=True)
+
+            # Save the file to the 'static/uploads/' folder
+            file.save(os.path.join(uploads_dir, file.filename))
+            photo = file.filename
+        else:
+            photo = None
+
+        # Render the user data on another HTML page
+        return render_template('t.html',
+                               name=name, email=email, phone=phone,
+                               whatsapp=whatsapp, instagram=instagram,
+                               facebook=facebook, twitter=twitter,
+                               website=website, address=address,
+                               business_name=business_name, photo=photo,about_bussiness=about_bussiness)
+
+if __name__ == '__main__':
+    app.run(debug=True)
